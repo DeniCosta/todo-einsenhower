530d401c5d7c8e8100a9e127cac11a73c350ceb4
DeniCosta <denisecosta.codes@gmail.com>
2023-10-11 20:55:15 -0300

    feat: Criação da arquitetura inicial
    Foi utilizado o Vite para a criação do projeto, adicionado a lib Styles Components e configurado a estilização global.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import GlobalStyle from './styles/GloblalStyles'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <GlobalStyle/>
+  )
+}
+
+export default App
