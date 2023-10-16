6c674e8c6d180f045adcad80415997538b9927ad
DeniCosta <denisecosta.codes@gmail.com>
2023-10-16 00:08:54 -0300

    feat: Criação da estilização do componente que adiciona uma tarefa

---
+++ src/components/views/landingpage/todoList/form/addTask.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const AddTaskStyled = styled.div`
+  width: 50%;
+
+  form{
+    display:flex;
+    width: 60%;
+    justify-content: start;
+    flex-wrap: nowrap;
+    align-items: center;
+  }
+
+  button{
+    background-color: #C4F2DE;
+    border: solid 1px #5BC294;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 2px;
+
+  }
+`;
+
+export default AddTaskStyled;
