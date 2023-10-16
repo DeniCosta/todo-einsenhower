88049e261b03440ebdc55d7d127994a621de6848
DeniCosta <denisecosta.codes@gmail.com>
2023-10-16 00:11:31 -0300

    feat: Criação da estilização do componente que realiza a busca das tarefas

---
+++ src/components/commons/filtros/search/search.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const SearchStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .searchIcon{
+    color: #301141;
+    background-color: #C0F2F2;
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    padding:3px;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export default SearchStyled;
