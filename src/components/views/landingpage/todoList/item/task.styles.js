248118629c39edb3b5e0e6bddf181cb063e6b82c
DeniCosta <denisecosta.codes@gmail.com>
2023-10-16 00:07:20 -0300

    feat: Criação da estilização do componente tarefa

---
+++ src/components/views/landingpage/todoList/item/task.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const TaskStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  padding: 10px;
+  margin: 5px auto;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 6px 4px 10px -3px rgba(0, 0, 0, 0.59);
+`;
+
+export default TaskStyled;
